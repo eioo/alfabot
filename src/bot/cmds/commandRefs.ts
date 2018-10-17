19cf280085f449d82d6ab15efff701389d34f35c
Kallu609 <khirvola@gmail.com>
2018-10-17 15:04:26 +0300

    added remindme command (db integration missing)

---
+++ src/bot/cmds/commandRefs.ts
@@ -0,0 +1,17 @@
+
+import HelpCommand from './help';
+import IBANCommand from './iban';
+import PostiCommand from './posti';
+import RemindmeCommand from './remindme';
+import WeatherCommand from './weather';
+
+import DebugCommand from './debug';
+
+export const commandRefs = [
+  WeatherCommand,
+  HelpCommand,
+  DebugCommand,
+  PostiCommand,
+  IBANCommand,
+  RemindmeCommand,
+]
