0898f525a41dc089118a204fabdc6a3dab391f71
Kallu609 <khirvola@gmail.com>
2018-10-17 15:03:16 +0300

    added remindme command (db integration missing)

--- src/bot/cmds/index.ts
+++ src/bot/cmds/index.ts
@@ -1,19 +1,6 @@
 import * as TelegramBot from 'node-telegram-bot-api';
 import CommandBase from './commandBase';
-import HelpCommand from './help';
-import IBANCommand from './iban';
-import PostiCommand from './posti';
-import WeatherCommand from './weather';
-
-import DebugCommand from './debug';
-
-const commands = [
-  WeatherCommand,
-  HelpCommand,
-  DebugCommand,
-  PostiCommand,
-  IBANCommand,
-];
+import { commandRefs } from './commandRefs';
 
 export const cmdList: CommandBase[] = [];
 
@@ -22,8 +9,8 @@ export function getCommand(cmdName: string): CommandBase | undefined {
 }
 
 export function load(bot: TelegramBot) {
-  for (const Command of commands) {
-    const cmd = new Command(bot);
+  for (const cmdClass of commandRefs) {
+    const cmd = new cmdClass(bot);
     cmd.listen && cmd.listen();
     cmdList.push(cmd);
   }
