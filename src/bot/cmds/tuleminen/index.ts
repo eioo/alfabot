b7ea84de9f93f9ce1033adbf2b7c7bfdbea7e4d2
naftis <pyry.rouvila@futurice.com>
2018-10-18 16:38:40 +0300

    add more answers to tuleminen

--- src/bot/cmds/tuleminen/index.ts
+++ src/bot/cmds/tuleminen/index.ts
@@ -1,4 +1,5 @@
 import CommandBase from 'bot/cmds/commandBase';
+import * as _ from 'lodash';
 import Bot from 'shared/types/bot';
 
 class TuleminenCommand extends CommandBase {
@@ -11,8 +12,20 @@ class TuleminenCommand extends CommandBase {
   }
 
   listen(): void {
-    this.onText(/(tulen|tulin|tulee)/i, async msg => {
-      this.reply(msg, 'paperia?? :D');
+    this.onText(/([^a-z]|.{0})tul(?!(li|va))|([^a-z]|.{0})tuu/i, async msg => {
+      const answers = [
+        'paperia?? :D',
+        'pabers?',
+        'tarviikko papruu :D',
+        '*tirsk*',
+      ];
+      const randomAnswer = _.sample(answers);
+
+      if (!randomAnswer) {
+        return;
+      }
+
+      this.reply(msg, randomAnswer);
     });
   }
 }
