2b37d249ff561bb36b6660fcda1e5b68998cd516
Kallu609 <khirvola@gmail.com>
2018-11-12 15:14:35 +0200

    no hard coding allowed!!!

--- src/bot/cmds/reps/index.ts
+++ src/bot/cmds/reps/index.ts
@@ -2,8 +2,7 @@ import CommandBase from 'bot/cmds/commandBase';
 import fetch from 'node-fetch';
 import Bot from 'shared/types/bot';
 
-// Fuck it :D
-const API_KEY = 'WXKut7bjGEN9aPuHYSCLEtrKPFvlzG8O';
+const API_KEY = process.env.GIPHY_TOKEN;
 
 class RepsCommand extends CommandBase {
   constructor(bot: Bot) {
@@ -14,6 +13,10 @@ class RepsCommand extends CommandBase {
 
   listen(): void {
     this.onText(/(reps|repesin|huutist?a?)/i, async msg => {
+      if (!API_KEY) {
+        return;
+      }
+
       const searchTerm = 'laugh';
       const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${searchTerm}&rating=R`;
       const request = await fetch(url);
