129dde32ea43b76294a16820e18c7ccffdf76565
Kallu609 <khirvola@gmail.com>
2018-11-14 08:19:16 +0200

    fixed circular import and made reps command better

--- src/panel/components/App.tsx
+++ src/panel/components/App.tsx
@@ -4,11 +4,6 @@ import { createGlobalStyle } from 'styled-components';
 import ChatSettings from './ChatSettings';
 import Denied from './Denied';
 
-const API_PORT = process.env.WEBSERVER_PORT || 3000;
-export const API_BASE = `${location.protocol}//${
-  location.hostname
-}:${API_PORT}/api`;
-
 const GlobalStyle = createGlobalStyle`
   @import url('//fonts.googleapis.com/css?family=Rubik&subset=latin');
 
