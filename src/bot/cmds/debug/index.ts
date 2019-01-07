641a848def1b216cbaeae27ffea6d0f2be5b4b81
Kallu609 <khirvola@gmail.com>
2019-01-08 01:34:15 +0200

    fixed /debug command to work with new schedule infratstructure

--- src/bot/cmds/debug/index.ts
+++ src/bot/cmds/debug/index.ts
@@ -1,7 +1,7 @@
 import CommandBase from 'bot/cmds/commandBase';
 import { schedules } from 'bot/schedules/rules';
-import { scheduledJobs } from 'node-schedule';
 import Bot from 'shared/types/bot';
+import { runSchedule } from 'bot/schedules';
 
 class DebugCommand extends CommandBase {
   constructor(bot: Bot) {
@@ -16,24 +16,25 @@ class DebugCommand extends CommandBase {
   listen(): void {
     this.onText(/^\/debug/i, async (msg, args, argCount) => {
       if (/^schedules?$/.test(args[0])) {
-        if (argCount === 1 || (argCount === 2 && args[1].toLowerCase() === 'list')) {
+        if (
+          argCount === 1 ||
+          (argCount === 2 && args[1].toLowerCase() === 'list')
+        ) {
           const names = schedules.map(s => s.name);
-          return this.reply(msg, [
-            `*List of schedules:*`,
-            ...names,
-          ]);
+          return this.reply(msg, [`*List of schedules:*`, ...names]);
         }
 
         if (args[1] === 'run' && argCount === 3) {
-          const schedule = scheduledJobs[args[2]];
+          const schedule = schedules.find(
+            s => s.name === args[2].toLowerCase()
+          );
 
           if (!schedule) {
-            this.reply(msg, 'Schedule not found.');
-            return;
+            return this.reply(msg, 'Schedule not found.');
           }
 
           this.reply(msg, `\`Running "${schedule.name}" schedule.\``);
-          return schedule.invoke();
+          return runSchedule(schedule.name, msg.chat.id);
         }
       }
 
