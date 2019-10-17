ce77030425fbf7d860a707db6e0440b2afb72198
Pyry Rouvila <pyry.rouvila@gmail.com>
2019-10-17 22:27:30 +0300

    Add triples

--- src/bot/cmds/doubles/index.ts
+++ src/bot/cmds/doubles/index.ts
@@ -1,15 +1,7 @@
 import CommandBase from 'bot/cmds/commandBase';
 import Bot from 'shared/types/bot';
+import { Roll, TriplesRoll } from './Roll';
 
-export class Roll {
-  roll = `${Math.floor(Math.random() * 100)}`.padStart(2, '0');
-
-  get isDoubles() {
-    return this.roll[0] === this.roll[1];
-  }
-}
-
-// tslint:disable-next-line: max-classes-per-file
 class DoublesCommand extends CommandBase {
   constructor(bot: Bot) {
     super(bot);
@@ -17,18 +9,37 @@ class DoublesCommand extends CommandBase {
   }
 
   listen(): void {
-    this.onText(/^(\/(doubles?|tuplat?)|tuplat päättää)/i, async msg => {
-      const reply = await this.reply(msg, '🎲🎲🎲 Rolling 🎲🎲🎲');
+    this.onText(
+      /^(\/(doubles?|tuplat?)|tuplat päättää|tuplilla)/i,
+      async msg => {
+        const reply = await this.reply(msg, '🎲🎲🎲 Rolling 🎲🎲🎲');
 
-      await new Promise(resolve => setTimeout(resolve, 2500));
+        await new Promise(resolve => setTimeout(resolve, 2500));
 
-      const roll = new Roll();
-      const response = roll.isDoubles
-        ? `🎲 \`[${roll.roll}]\` You rolled doubles! :))`
-        : `🎲 \`[${roll.roll}]\` No doubles for u :((`;
+        const roll = new Roll();
+        const response = roll.isOnlySameNumbers
+          ? `🎲 \`[${roll.roll}]\` You rolled doubles! :))`
+          : `🎲 \`[${roll.roll}]\` No doubles for u :((`;
 
-      this.editReply(reply, response);
-    });
+        this.editReply(reply, response);
+      }
+    );
+
+    this.onText(
+      /^(\/(triples?|triplat?)|triplat päättää|triploilla)/i,
+      async msg => {
+        const reply = await this.reply(msg, '🎲🎲🎲 Rolling 🎲🎲🎲');
+
+        await new Promise(resolve => setTimeout(resolve, 2500));
+
+        const roll = new TriplesRoll();
+        const response = roll.isOnlySameNumbers
+          ? `🎲 \`[${roll.roll}]\` You rolled triples! :))`
+          : `🎲 \`[${roll.roll}]\` No triples for u :((`;
+
+        this.editReply(reply, response);
+      }
+    );
   }
 }
 
